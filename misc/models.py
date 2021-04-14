9ecadd196270c4b7118c537cb1a95850d64b8b5c
Groompel <kzdoandos@gmail.com>
2021-04-14 17:27:20 +0600

    add Room model

--- misc/models.py
+++ misc/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=50)
+
+    class Meta:
+        verbose_name = 'Room'
+        verbose_name_plural = 'Rooms'
+
+    def __str__(self):
+        return self.name
