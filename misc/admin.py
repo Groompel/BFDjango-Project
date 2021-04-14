fa6c5c04827b5f9bb3d92c4950f44d17bf5482b6
Groompel <kzdoandos@gmail.com>
2021-04-14 18:46:39 +0600

    add models to admin

--- misc/admin.py
+++ misc/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Room, BusinessCenter, ResidentialComplex
 
 # Register your models here.
+
+admin.site.register(Room)
+admin.site.register(BusinessCenter)
+admin.site.register(ResidentialComplex)
--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CommercialProperty, HouseProperty, FlatProperty
 
 # Register your models here.
+
+admin.site.register(CommercialProperty)
+admin.site.register(HouseProperty)
+admin.site.register(FlatProperty)
