94a8c5c8faee70d1b9d5967da00c5cef9959ec5a
Groompel <kzdoandos@gmail.com>
2021-04-14 17:54:52 +0600

    create HouseProperty model

--- properties/models.py
+++ properties/models.py
@@ -25,7 +25,7 @@ class AbstractLivingProperty(AbstractProperty):
     kitchen_area = models.FloatField(
         verbose_name='Kitchen area', blank=False, null=False, default=0)
     construction_type = models.CharField(
-        verbose_name='Construction type', blank=False, null=False, default='')
+        verbose_name='Construction type', blank=False, null=False, default='', max_length=100)
 
     class Meta:
         abstract = True
@@ -41,3 +41,17 @@ class CommercialProperty(AbstractProperty):
 
     def __str__(self):
         return self.business_center.name
+
+
+class HouseProperty(AbstractLivingProperty):
+    land_area = models.FloatField(
+        verbose_name='Land area', blank=False, null=False, default=0)
+    number_of_floors = models.PositiveIntegerField(
+        verbose_name='Number of floors', blank=False, null=False, default=0)
+
+    class Meta:
+        verbose_name = 'House property'
+        verbose_name_plural = 'House properties'
+
+    def __str__(self):
+        return '%s, %i floors' % (self.address, self.number_of_floors)
