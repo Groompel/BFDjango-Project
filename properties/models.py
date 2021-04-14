c3a86b3ad9ee55b73b9e315dbd1f2c4d37b84f80
Groompel <kzdoandos@gmail.com>
2021-04-14 17:19:27 +0600

    create AbstractPropertyModel

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AbstractProperty(models.Model):
+    price = models.FloatField(verbose_name='Price',
+                              blank=False, null=False, default=0)
+    area = models.FloatField(
+        verbose_name='Area', blank=False, null=False, defautl=0)
+    built_year = models.IntegerField(
+        verbose_name='Year', blank=False, null=False, default=2000)
+    exploitation_year = models.IntegerField(
+        verbose_name='Exploitation year', blank=False, null=False, default=2000)
+
+    class Meta:
+        abstract = True
