3839ae17089a7cdfeeef34f5e6262a6ab6e78e38
Groompel <kzdoandos@gmail.com>
2021-04-14 17:40:10 +0600

    add address and room fields to AbstractProperty model

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,4 @@
+from misc.models import Address, Room
 from django.db import models
 
 # Create your models here.
@@ -12,6 +13,9 @@ class AbstractProperty(models.Model):
         verbose_name='Year', blank=False, null=False, default=2000)
     exploitation_year = models.IntegerField(
         verbose_name='Exploitation year', blank=False, null=False, default=2000)
+    address = models.ForeignKey(
+        Address, verbose_name='Address', on_delete=models.CASCADE)
+    rooms = models.ManyToManyField(Room, verbose_name='Rooms')
 
     class Meta:
         abstract = True
