8ffa82117a6c4086b1134cbf47efecc7d4bc2e3d
Groompel <kzdoandos@gmail.com>
2021-04-14 15:44:14 +0600

    connect postgresql

--- shanyrak/settings.py
+++ shanyrak/settings.py
@@ -73,11 +73,31 @@ WSGI_APPLICATION = 'shanyrak.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'shanyrak',
+
+        'USER': 'shanyrak_admin',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
     }
+
 }
 
 
