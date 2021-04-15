be6b184f56450a4a5542ccb81140343a85ba24da
Groompel <kzdoandos@gmail.com>
2021-04-15 13:04:42 +0600

    create DefaultUser model

--- _auth/models.py
+++ _auth/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class DefaultUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(
+        verbose_name='Phone number', max_length=40, blank=False, null=False, default='+7')
+    first_name = models.CharField(
+        verbose_name='First name', max_length=100, blank=False, null=False, default='')
+    last_name = models.CharField(
+        verbose_name='Last name', max_length=100, blank=False, null=False, default='')
+    GENDERS = [('male', 'Male'),  ('female', 'Female')]
+    gender = models.CharField(verbose_name='Gender',
+                              choices=GENDERS, default='male', max_length=6)
+    birth_date = models.DateField(verbose_name='Birth date')
