ac3a60ba0e0ff11e94c6ce8ac8d757c0692522a4
Groompel <kzdoandos@gmail.com>
2021-04-16 13:46:36 +0600

    alter model options on _auth app

--- _auth/models.py
+++ _auth/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class DefaultUser(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(
         verbose_name='Phone number', max_length=40, blank=False, null=False, default='+7')
     first_name = models.CharField(
@@ -17,11 +17,32 @@ class DefaultUser(models.Model):
                               choices=GENDERS, default='male', max_length=6)
     birth_date = models.DateField(verbose_name='Birth date')
 
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return '%s, %s %s' % (self.user.email, self.first_name, self.last_name)
+
 
 class Agency(models.Model):
     name = models.CharField(verbose_name='Name',
                             max_length=250, blank=False, null=False, default='')
 
+    class Meta:
+        verbose_name = 'Agency'
+        verbose_name_plural = 'Agencies'
+
+    def __str__(self):
+        return self.name
+
 
 class Agent(DefaultUser):
     agency = models.ForeignKey(Agency, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Agent'
+        verbose_name_plural = 'Agents'
+
+    def __str__(self):
+        return self.agency.name
